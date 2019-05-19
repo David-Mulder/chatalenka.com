e589ecf2168ba74a931fac0140e6a5d122d116ee
6ft6 <50442067+6ft6@users.noreply.github.com>
2019-05-19 15:22:02 +0200

    Update imagesize.js
    undone prev commit

--- layouts/helpers/imagesize.js
+++ layouts/helpers/imagesize.js
@@ -2,5 +2,5 @@ module.exports = function (str, big) {
     if (typeof big === 'number') {
         return str.replace('.jpg', '.' + big + '.jpg')
     }
-    return !big ? str.replace('.jpg', '.820.jpg') : str.replace('.jpg', '.1230.jpg')
+    return !big ? str.replace('.jpg', '.820.jpg') : str.replace('.jpg', '.1640.jpg')
 }
