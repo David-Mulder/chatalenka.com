cbbd9b468d3d66a826896bb51122002c4f079d0e
6ft6 <50442067+6ft6@users.noreply.github.com>
2019-05-19 14:48:15 +0200

    Update translate.js
    Dutch added

--- layouts/helpers/translate.js
+++ layouts/helpers/translate.js
@@ -1,12 +1,14 @@
 const yaml = require('yaml-js');
 const fs = require('fs');
 const englishFile = fs.readFileSync('./src/en/strings.md', 'UTF-8'); 
-const slovakFile = fs.readFileSync('./src/sk/strings.md', 'UTF-8'); 
+const slovakFile  = fs.readFileSync('./src/sk/strings.md', 'UTF-8'); 
+const dutchFile   = fs.readFileSync('./src/nl/strings.md', 'UTF-8'); 
 module.exports = function (locale, stringOrArray) {
     const path = typeof stringOrArray === 'string' ? [stringOrArray] : stringOrArray;
     const strings = {
         en: yaml.load_all(englishFile)[0],
-        sk: yaml.load_all(slovakFile)[0]
+        sk: yaml.load_all(slovakFile)[0],
+        nl: yaml.load_all(dutchFile)[0]
     };
     return path.reduce((prev, cur) => {
         return prev[cur];
